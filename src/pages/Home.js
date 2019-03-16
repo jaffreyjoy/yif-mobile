a016f2ddc1f616754f2472ad791c2d285ecbe691
Jaffrey98 <jaffreyjoy@gmail.com>
2019-03-17 03:32:42 +0530

    [enh] improve ui of home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
 import { Button } from 'react-native-paper';
 import { Actions } from 'react-native-router-flux';
@@ -18,9 +19,14 @@ export default class MainHome extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to yif mobile portal</Text>
-        <Button onPress={() => Actions.login()}>Login</Button>
-        <Button onPress={() => Actions.register()}>Register</Button>
+        <View style={styles.topSec}>
+          <Image style={ styles.logo } source={require("../../assets/images/yif-logo.png")} />
+          <Text style={styles.welcome}>Welcome to yif mobile portal</Text>
+        </View>
+        <View style={styles.bottomSec}>
+          <Button color='#2E303C' style={ styles.login } onPress={() => Actions.login()}>Login</Button>
+          <Button color='#2E303C' style={ styles.signup } onPress={() => Actions.register()}>Register</Button>
+        </View>
       </View>
     );
   }
@@ -33,10 +39,35 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#2E303C',
   },
+  topSec:{
+    flex: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // borderWidth: 2,
+  },
+  logo:{
+    marginBottom: 20,
+  },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    // padding: 10,
     color: '#ffffff',
   },
+  bottomSec:{
+    flex: 2.5,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    // borderWidth: 2,
+  },
+  login:{
+    backgroundColor: '#A7D24A',
+    margin: 10,
+    width: 150
+  },
+  signup:{
+    backgroundColor: '#A7D24A',
+    margin: 10,
+    width: 150
+  }
 });
